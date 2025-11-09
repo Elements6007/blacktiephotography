5119df31aa1e361161c0f1777409f4a2d58d7dbe
John Quigley <Elements6007@gmail.com>
2025-11-09 03:18:39 -0500

    fix mobile

---
+++ scripts/navbar.js
@@ -0,0 +1,19 @@
+var nav = document.querySelector('nav');
+if (document.URL.indexOf("projects") > -1) {
+    window.addEventListener('scroll', function () {
+        if (window.pageYOffset > 5) {
+            nav.classList.add('bg-dark', 'shadow');
+        } else {
+            nav.classList.remove('bg-dark', 'shadow');
+        }
+    });
+} else {
+    window.addEventListener('scroll', function () {
+        if (window.pageYOffset > 5) {
+            nav.classList.add('bg-dark', 'shadow');
+
+        } else {
+            nav.classList.remove('bg-dark', 'shadow');
+        }
+    });
+}
