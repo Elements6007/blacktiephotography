c8ffc28b6a9966954c0be3bddf30e2c7f0a147f7
Elements6007 <Elements6007@gmail.com>
2026-05-06 03:49:49 -0400

    Candid 30.1.2
    Navbar burger menu white response

--- scripts/navbar-short.js
+++ scripts/navbar-short.js
@@ -1,7 +1,7 @@
 var newImageUrl = Logo.src;
 var urlLocation;
 var nav = document.querySelector('nav');
-
+var span = document.querySelector('span');
 if (document.URL.indexOf("projects") > -1) {
     window.addEventListener('scroll', function () {
         if (window.pageYOffset > 50) {
@@ -9,12 +9,14 @@ if (document.URL.indexOf("projects") > -1) {
             newImageUrl = "images/logo_nobackground_crop.png";
             const links = nav.getElementsByClassName("nav-link");
             navAdapt(links, "#FFFFFF");
+            span.classList.remove('navbar-dark-icon');
 
         } else {
             nav.classList.remove('bg-dark', 'shadow');
             newImageUrl = "images/logo_nobackground_crop_black.png";
             const links = nav.getElementsByClassName("nav-link");
             navAdapt(links, "#000000");
+            span.classList.add('navbar-dark-icon');
 
         }
 
@@ -27,12 +29,14 @@ if (document.URL.indexOf("projects") > -1) {
             newImageUrl = "images/logo_nobackground_crop.png";
             const links = nav.getElementsByClassName("nav-link");
             navAdapt(links, "#FFFFFF");
-            
+            span.classList.remove('navbar-dark-icon');
+
         } else {
             nav.classList.remove('bg-dark', 'shadow');
             newImageUrl = "images/logo_nobackground_crop_black.png";
             const links = nav.getElementsByClassName("nav-link");
             navAdapt(links, "#000000");
+            span.classList.add('navbar-dark-icon');
 
         }
 
